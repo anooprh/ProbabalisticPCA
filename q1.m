f1a13984705c9d5c512aa8155530efe74fc0857a
anooprh <anoop.hallur@gmail.com>
2014-04-29 21:15:30 -0400

    completed problem 2 first part I think

--- q1.m
+++ q1.m
@@ -13,7 +13,7 @@ eigen_values = diag(Lambda);
 eigen_values_increasing = fliplr(eigen_values')';
 plot(fliplr(sqrt(eigen_values_increasing)));
 hold on
-plot(fliplr(sqrt(eigen_values_increasing)), '*k');
+plot(fliplr(sqrt(eigen_values_increasing)), '*');
 xlabel('Component Number');
 ylabel('sqrt - eigen value');
 title('Square Rooted Eigen Value Spectrum');
@@ -32,7 +32,17 @@ mean_repeated = repmat(mean_spike_counts, NUM_TRIALS, 1);
 Z1 = PC1' * (Xplan - mean_repeated)';
 Z2 = PC2' * (Xplan - mean_repeated)';
 Z3 = PC3' * (Xplan - mean_repeated)';
-plot3(Z1, Z2, Z3, '.');
+plot3(Z1(91*0+1:91*1), Z2(91*0+1:91*1), Z3(91*0+1:91*1), '.b');hold on;
+plot3(Z1(91*1+1:91*2), Z2(91*1+1:91*2), Z3(91*1+1:91*2), '.r');hold on;
+plot3(Z1(91*2+1:91*3), Z2(91*2+1:91*3), Z3(91*2+1:91*3), '.g');hold on;
+plot3(Z1(91*3+1:91*4), Z2(91*3+1:91*4), Z3(91*3+1:91*4), '.c');hold on;
+plot3(Z1(91*4+1:91*5), Z2(91*4+1:91*5), Z3(91*4+1:91*5), '.k');hold on;
+plot3(Z1(91*5+1:91*6), Z2(91*5+1:91*6), Z3(91*5+1:91*6), '.m');hold on;
+plot3(Z1(91*6+1:91*7), Z2(91*6+1:91*7), Z3(91*6+1:91*7), '.y');hold on;
+plot3(Z1(91*7+1:91*8), Z2(91*7+1:91*8), Z3(91*7+1:91*8), '.','Color',[1,0.4,0.6]);hold on;
+legend('Reaching angle 1','Reaching angle 2','Reaching angle 3', ...
+    'Reaching angle 4','Reaching angle 5','Reaching angle 6',...
+    'Reaching angle 7','Reaching angle 8');
 xlabel('1st Component Score');
 ylabel('2nd Component Score');
 zlabel('3rd Component Score');
--- q2_b.m
+++ q2_b.m
@@ -1,28 +1,57 @@
 load q2.mat
 
-sigma_sqr = 1;
+LINE_LENGTH = 15;
+sigma_sqr = 0.5;
 mu_bkp = mu;
-W = U1;
+W = [1;1];
+n_iter = 100;
+L = zeros(1,n_iter);
 
-L = 0;
+for k =1:n_iter
+    C = W*W' + sigma_sqr*eye(HIGH_DIMENSION);
 
-C = W*W' + sigma_sqr*eye(HIGH_DIMENSION);
+    % E step
+    mu_rep = repmat(mu, [1, NUMBER_OF_POINTS]);
+    z_x_mu = W'*inv(C)*(X-mu_rep);
+    z_x_sigma = eye(LOW_DIMENSION) - W'*inv(C)*W;
 
-% Calculating log likelihood
-for i = 1:NUMBER_OF_POINTS
-    L = L + logmvnpdf(X(:,i), mu, C);
+    % M Step
+    E_zzt = z_x_sigma + z_x_mu * z_x_mu';
+%     E_zzt = cov(z_x_sigma,z_x_sigma') + z_x_mu * z_x_mu';
+
+    term1 = (X-mu_rep) * z_x_mu';
+    term2 = inv(sum(repmat(E_zzt, [1, NUMBER_OF_POINTS])));
+%     term2 = inv(E_zzt);
+    W = term1 * term2;
+
+    term3 = (X-mu_rep)*(X-mu_rep)';
+    term4 = W*(z_x_mu*(X-mu_rep)');
+    sigma_sqr = trace(term3 - term4)/ ...
+        (NUMBER_OF_POINTS * HIGH_DIMENSION);
+
+    % Calculating log likelihood
+    for i = 1:NUMBER_OF_POINTS
+        L(k) = L(k) + logmvnpdf(X(:,i), mu, C);
+    end
 end
+plot(L);
 
+% Question 2c
+plot (X(1,:), X(2,:), 'k.', 'markersize',30);
+hold on;
+plot (mu(1), mu(2), 'g.', 'markersize',45);
+hold on;
+point_1_for_plot = mu - LINE_LENGTH * W;
+point_2_for_plot = mu + LINE_LENGTH * W;
+line([point_1_for_plot(1), point_2_for_plot(1)],...
+    [point_1_for_plot(2), point_2_for_plot(2)], 'Color', 'k');
+hold on;
 
-
-% E step
-mu_rep = repmat(mu, [1, NUMBER_OF_POINTS]);
-z_x_mu = W'*inv(C)*(X-mu_rep);
-z_x_sigma = eye(LOW_DIMENSION) - W'*inv(C)*W;
-
-E_zzt = z_x_sigma + z_x_mu * z_x_mu';
-
-term1 = (X-mu_rep) * z_x_mu';
-term2 = inv(sum(repmat(E_zzt, [1, NUMBER_OF_POINTS])));
-
-W = term1 * term2;
+X_cap = W*z_x_mu + mu_rep;
+plot (X_cap(1,:), X_cap(2,:), 'r.', 'markersize',30);
+hold on;
+for i=1:NUMBER_OF_POINTS
+    line([X_cap(1, i), X(1, i) ], [X_cap(2, i), X(2, i) ], 'Color', 'r');
+    hold on
+end
+axis equal
